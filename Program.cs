ecc476387410a659b6f11b12c82b9fbc66a8a22a
enot demiurge <enotstone@ya.ru>
2022-07-31 23:07:55 +0300

    14

--- Program.cs
+++ Program.cs
@@ -4,12 +4,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ConsoleApp14
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+           
+
+            Cat cat1 = new Cat() { Alias = "Iris" };
+            cat1.ShowInfo();
+            Dog dog1 = new Dog();
+            dog1.ShowInfo();
+            Console.ReadKey();
         }
     }
+
+    public abstract class Animal
+    {
+        public string Genus { get; set; }
+        public Animal() 
+            {
+                Genus = "Name";
+            }
+        public abstract void Say();
+        public void ShowInfo()
+        {
+            Console.Write("Genus {0}, ", Genus);
+            Say();
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public string Alias { get; set; }
+        public Cat()
+        {
+            Genus = "Cat";
+            Alias = "NickNameCat unknown";
+        }
+        public override void Say()
+        {
+            
+            Console.WriteLine("Alias {0} say: meow ", Alias);
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public string Alias { get; set; }
+        public Dog()
+        {
+            Genus = "Dog";
+            Alias = "NickNameDog unknown";
+        }
+        public override void Say()
+        {
+
+            Console.WriteLine("Alias {0} say: woof ", Alias);
+        }
+    }
+
 }
